073bcaea86d7ac136f2b8f95f3b6e988356795e5
Islombek <eislombek9599@gmail.com>
2023-10-11 22:14:07 +0300

    Feat: Task I

--- train.js
+++ train.js
@@ -1,12 +1,25 @@
+// let arr = [];
+
+// function reverseArr(array) {
+//   arr = array;
+
+//   return array.reverse();
+// }
+
+// const result = reverseArr(["apple", "mango", "grape", "ananas"]);
+// console.log(result);
+
 //I-Task: arrayning ichidagi 0 index qiymatni arrayning oxiriga qoyib return qilsin
 
-let arr = [];
-
-function reverseArr(array) {
-  arr = array;
-
-  return array.reverse();
+function reverserZeroIndex(arr) {
+  if (arr.length > 0) {
+    const arrZeroIndex = arr.shift();
+    arr.push(arrZeroIndex);
+  }
+  return arr;
 }
 
-const result = reverseArr(["apple", "mango", "grape", "ananas"]);
-console.log(result);
+const originalArray = ["apple", "mango", "grape", "grante", "peach"];
+const modifiedArray = reverserZeroIndex(originalArray);
+
+console.log(modifiedArray);
